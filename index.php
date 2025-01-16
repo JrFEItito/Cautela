9560bf512efa7cd73608c1af4b995b8aaf7ab63e
IgorPieralini <igorpieralini@gmail.com>
2025-01-15 21:39:35 -0300

    atualizacao index-php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cautela</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="style/index.css">
+    </head>
+
+
+    <body>
+        <header>
+
+            <!-- CABECALHO -->
+
+        </header>
+
+        <main>
+
+            <!-- SITE -->
+
+            <!-- inicio -->
+            <section>
+
+            </section>
+
+            <!-- Voce Sabia Que -->
+            <section>
+
+            </section>
+
+            <!-- Paulo Ferreira -->
+            <section>
+
+            </section>
+
+            <!-- Nossos Servivos -->
+            <section>
+
+            </section>
+
+            <!-- Nossa Atuacao -->
+            <section>
+
+            </section>
+
+            <!-- Noticias -->
+            <section>
+
+            </section>
+
+        </main>
+
+        <footer>
+            <!-- RODAPÉ -->
+            <!-- INICIA APÓS NOTICIAS -->
+
+
+        </footer>
+    </body>
+
+    <script type="text/javascript" src="script/index.js"></script>
+
+</html>
