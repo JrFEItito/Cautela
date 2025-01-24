2cb1587899ade9607bd3c599242416d8029139d7
IgorPieralini <igorpieralini@gmail.com>
2025-01-24 20:16:57 -0300

    update

--- Portal/Login/login.php
+++ Portal/Login/login.php
@@ -1,4 +1,5 @@
 <?php
+global $mysqli;
 include('conexao.php');
 
 if (isset($_POST['email']) && isset($_POST['senha'])) {
@@ -10,7 +11,9 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
         $email = $mysqli->real_escape_string($_POST['email']);
         $senha = $mysqli->real_escape_string($_POST['senha']);
 
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_code = "SELECT * FROM usuarios 
+                     WHERE email = '$email' AND senha = '$senha'";
+
         $sql_query = $mysqli->query($sql_code) or die("Falha na execucao do código SQL: " . $mysqli->error);
 
         $quantidade = $sql_query->num_rows;
@@ -44,20 +47,20 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
     <title>Portal Cautela</title>
 </head>
 <body>
+
+
     <div id="fundo">
     <div id="logo_cautela">
         <img src="../../imagens/Logo completo 1-Escuro.png" alt="Logo Cautela">
     </div>
     
     <div id="login">
-     <form action="../Sistema/portal.php" method="post" class="formulario">
+     <form action="login.php" method="post" class="formulario">
                 <label for="login">Login</label>
-                    <input type="email" placeholder="E-mail" name="email" require id="id_email">
-                    <input type="password" placeholder="Senha" name="senha" require id="id_senha" >
+                    <input type="email" placeholder="E-mail" name="email" required id="id_email">
+                    <input type="password" placeholder="Senha" name="senha" required id="id_senha" >
                     <input type="submit" placeholder="Entrar" id="id_submit">
             </form>
-
-
     </div> 
     
     </div>
--- Portal/Login/protect.php
+++ Portal/Login/protect.php
@@ -1,10 +1,10 @@
-<?php 
-    if(!isset($_SESSION)){
-        session_start();
-    }
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
 
-    if(!isset($_SESSION['id'])){
-        die("Voce nao pode acessar essa página, porque nao esta logado. <p><a href=\"../Login/login.php\">Entrar</a></p>");
-    }
-
+if (!isset($_SESSION['id'])) {
+    header("Location: ../Login/login.php");
+    exit();
+}
 ?>
